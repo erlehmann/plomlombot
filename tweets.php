9d530eb0ca933dcb41bec6a461379377a111038d
Matthias Rampke <matthias@rampke.de>
2009-09-26 16:50:13 +0200

    sauce

--- tweets.php
+++ tweets.php
@@ -1,4 +1,5 @@
 <?php
+/* from http://groups.google.com/group/twitter-development-talk/browse_thread/thread/1df7a2d9898d93e4 */
      $page_num = 1;
      $txtString = "";
      while ($page_num <= 10 )
