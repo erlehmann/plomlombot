1e05f093bdba542a4fb271b4926d38ab693961b3
Matthias Rampke <matthias@rampke.de>
2009-09-26 16:34:59 +0200

    fetch more tweets

--- tweets.php
+++ tweets.php
@@ -3,7 +3,7 @@
      $txtString = "";
      while ($page_num <= 10 )
      {
-         $host = "http://search.twitter.com/search.atom?q=from%3Aplomlompom";
+         $host = "http://search.twitter.com/search.atom?q=from%3Aplomlompom&page=$page_num&rpp=100";
          $result = file_get_contents($host);
          $xml = new SimpleXMLElement($result);
          foreach ($xml->entry as $entry)
